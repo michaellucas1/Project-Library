2dc94773f213e6202cc7bbcc813e9e5a6971bcac
John <141792618+michaellucas2008@users.noreply.github.com>
2024-08-28 16:53:57 -0400

    add event listeners to buttons and form to save the new  book entry data to a javascript object

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-cd
+const myLibrary=[];
+
+function Book(){
+
+}
+function AddToLibrary(){}
+
+const displayModal = document.querySelector(".display-modal");
+const dialog =document.querySelector("dialog");
+const closeModal=document.querySelector(".close-modal");
+const form=document.querySelector('.book-form');
+
+
+form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    console.dir(event);
+    const myFormData = new FormData(event.target);
+    
+    const formDataObject = {};
+    myFormData.forEach((value,key)=>(formDataObject[key]=value));
+    console.log(formDataObject);
+});
+displayModal.addEventListener("click",()=>{
+    dialog.showModal();
+});
+closeModal.addEventListener("click", ()=>{
+    dialog.close();
+});
